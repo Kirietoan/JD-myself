e2edac2d756cc8a4a3fce1641f14f498aba63837
树叶 <38684887+shuye71@users.noreply.github.com>
2021-01-15 01:27:11 +0800

    Update jd_jdfactory.js

--- scripts/jd_jdfactory.js
+++ scripts/jd_jdfactory.js
@@ -622,7 +622,9 @@ function jdfactory_getHomeData() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `http://api.turinglabs.net/api/v1/jd/ddfactory/read/${randomCount}/`, timeout: 10000}, (err, resp, data) => {
+    $.get({url: "https://gitee.com/soundshuye/Some-ID/blob/master/jdfactory.json",headers:{
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }}, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
