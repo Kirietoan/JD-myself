d86d125f8b0a14116beaadc853a14c97318244e3
树叶 <38684887+shuye71@users.noreply.github.com>
2021-01-15 15:28:23 +0800

    Update jd_cash.js

--- scripts/jd_cash.js
+++ scripts/jd_cash.js
@@ -27,7 +27,7 @@ let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭�
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
-const randomCount = 1;
+const randomCount = 3 ;
 const inviteCodes = [
   `Ihsza-6yZP8u7GvRy3MU3g@9YWMtUcosWudJONU@9YWMtUcosWudJONU@95OxuU0Ss1qcJeZTH_uV@eU9YaOi6NPx19WfUwyYa1A@eU9Yae_hYK0m9maDniYR1g@eU9YaOi6NPx19WfUwyYa1A`,
   `Ihsza-6yZP8u7GvRy3MU3g@9YWMtUcosWudJONU@9YWMtUcosWudJONU@95OxuU0Ss1qcJeZTH_uV@eU9YaOi6NPx19WfUwyYa1A@eU9Yae_hYK0m9maDniYR1g@eU9YaOi6NPx19WfUwyYa1A`
